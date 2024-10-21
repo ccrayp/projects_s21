48085eee9082d4d0dbf0b66ebbd116a5744be27a
roman@M1402 <demixhd2@gmail.com>
2024-10-21 20:14:40 +0300

    Linux shell

---
+++ T01D01/src/quest3.sh
@@ -0,0 +1,7 @@
+mv door_management_fi door_management_files
+mkdir door_configuration
+mv *conf door_configuration
+mkdir door_map
+mv door_map_1.1 door_map
+mkdir door_log
+mv *log door_log
---
+++ T01D01/src/quest6.sh
@@ -0,0 +1,7 @@
+ chmod +x ./ai_help/keygen.sh
+ chmod +x ./ai_help/unifier.sh
+
+cd ./ai_help
+./keygen.sh
+rm ./key/file*
+./unifier.sh
